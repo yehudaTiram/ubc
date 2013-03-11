d7b8d5225c82705928011df5345454da77fdb092
Bethany Martin <bmartinstudio@gmail.com>
2013-03-11 19:00:27 -0400

    Add videos to homepage

--- page-homepage.php
+++ page-homepage.php
@@ -9,7 +9,7 @@ Template Name: Homepage 2013
 		<section id="primary" class="full-width">
 			<div id="content" role="main">
 
-				<?php the_post(); ?>
+				<section id="hp-riders"><?php the_post(); ?>
 
 				<header class="page-header">
 					<h2 class="page-title">Featured Riders</h2>
@@ -45,6 +45,48 @@ Template Name: Homepage 2013
 				</div><!-- #<?php echo $level; ?> -->
 				
 				<?php } // end foreach loop ?>
+</section>				
+				
+				
+				<section id="hp-videos"><header class="page-header">
+					<h2 class="page-title">Videos</h2>
+				</header>
+				<?php
+				  $wpvq = array( 'post_type' => 'videos',
+				  				 'rider' => 'homepage' );
+				  $rider_posts = new WP_Query ($wpvq);
+				  $i = 0;
+				?>
+					          
+					  <?php foreach( $rider_posts->posts as $post ) : $i++; ?>
+					   <div class="span-8<?php if ($i == 3) { echo ' last'; } ?>">
+							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<div class="video-archive">
+								<header class="entry-header">
+									<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'united' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+								</header><!-- .entry-header -->
+								<div class="entry-content">
+									<?php
+										printf( __( '<time class="entry-date" datetime="%1$s" pubdate> <span class="month">%2$s</span> <span class="day">%3$s</span> <span class="year">%4$s</span></time>', 'united' ),
+											get_the_date( 'c' ),
+											get_the_date('M'), // Jan, Feb, etc
+											get_the_date('d'), // 01-31
+											get_the_date('Y') // 1979, etc
+										);
+									?>
+									<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'united' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_post_thumbnail('310x175'); ?></a>
+								</div><!-- .entry-content -->
+								<footer class="entry-meta">
+									<?php the_date(); ?>
+								</footer>
+								</div><!-- .video-archive -->
+							</article><!-- #post-<?php the_ID(); ?> -->
+						</div><!-- .span-8 -->
+						<?php if ($i == 3) { ?><hr class="space" /><?php $i = 0; } ?>
+					 <?php endforeach ?>
+</section>				
+				
+				
 
 			</div><!-- #content -->
 		</section><!-- #primary -->
