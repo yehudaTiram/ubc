8fff0411c6dc5686a8969f6f6124226f3bdb229a
Bethany Martin <bmartinstudio@gmail.com>
2013-03-11 17:08:52 -0400

    Pop Header out of the main page div

--- header.php
+++ header.php
@@ -37,8 +37,8 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed container">
-	<header id="branding" role="banner">
+<div id="header-wrap">
+<header id="branding" role="banner">
 			<hgroup>
 				<h1 id="site-title"><span><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
 				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
@@ -62,6 +62,9 @@
 				<?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?>
 			</nav><!-- #access -->
 	</header><!-- #branding -->
+</div>
+<div id="page" class="hfeed container">
+	
 
 
 	<div id="main">
