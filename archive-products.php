4586596aba04d458868e8d54a57deb9615b8c9c7
Bethany Martin <bmartinstudio@gmail.com>
2013-05-16 21:14:09 -0400

    New Product Archive Pages
    This update relies on the use of this Category Images plugin:
    http://wordpress.org/extend/plugins/categories-images/

--- archive-products.php
+++ archive-products.php
@@ -1,16 +1,49 @@
 <?php get_header(); ?>
 
-		<section id="primary">
+		<section id="primary"  class="full-width">
 			<div id="content" role="main">
 
 				<header class="page-header">
 					<h1 class="page-title">Products</h1>
 				</header>
+<div id="product-categories">
+				<?php
+$terms = get_terms('type','hide_empty=1');
+ $count = count($terms);
+ if ( $count > 0 ){
+     echo "<ul>";
+     foreach ( $terms as $term ) {
+       	
+       echo "<li><a href='" . get_term_link($term->slug, 'type') . "' title='" . $term->name . "' >";
+       
+if (z_taxonomy_image_url($term->term_id) != '') {
 
-				<?php get_template_part('slideshow-products'); ?>
+       echo "<img src='" . z_taxonomy_image_url($term->term_id) . "' title='" . $term->name . "' />";
+
+   } else {
+   		echo "<img src='";
+   		echo bloginfo('stylesheet_directory') . "/images/image-coming-soon.gif' title='" . $term->name . "' />";
+   };
+       echo $term->name . "</a></li>"; 	
+       
+        
+     }
+     echo "</ul>";
+ }
+ ?>
+
+<div class="clear"></div>
+
+</div>
+
+
+			
+
+
+
 
 			</div><!-- #content -->
 		</section><!-- #primary -->
 		
-<?php get_sidebar('products'); ?>
+
 <?php get_footer(); ?>
---
+++ taxonomy-type.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+		<section id="primary" class="full-width">
+			<div id="content" role="main">
+
+				<?php the_post(); ?>
+
+				<header class="page-header">
+					<h1 class="page-title"><?php 
+
+					$term =	$wp_query->queried_object;
+					echo $term->name;
+					?>
+						
+					</h1>
+				</header>
+
+				<?php rewind_posts(); ?>
+				
+				<div id="taxonomy-archive-type">
+					<ul>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+				
+				<li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+						<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( '%s', 'united' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_post_thumbnail('310x175'); ?><?php echo get_the_title(); ?></a>
+
+							
+					</li><!-- #post-<?php the_ID(); ?> -->
+				
+				
+					
+					
+
+				<?php endwhile; ?>
+				</ul>
+				</div>
+				
+			</div><!-- #content -->
+		</section><!-- #primary -->
+
+<?php get_footer(); ?>
