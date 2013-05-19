d70e6f9062c47ca4fa75d60c9786285ad0bc7241
D- G-H <daveghall@gmail.com>
2013-05-18 23:54:30 -0400

    Added related products items on single-products page, adjusted the reflow as necessary

--- single-products.php
+++ single-products.php
@@ -1,41 +1,44 @@
+<?php global $post;?>
+
 <?php get_header(); ?>
 
 		<div id="primary"   class="full-width">
 			<div id="content" role="main">
 
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-				
+
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
+
 					<header class="entry-header">
 						<h1 class="entry-title"><?php the_title(); ?></h1>
 					</header><!-- .entry-header -->
-					
+
 					<div class="entry-content">
-				
+
 						<?php united_slideshow('large'); ?>
-						
+
 						<?php the_content(); ?>
-						
-<?php 
+
+<?php
 						$meta = $product_metabox->the_meta();
 						if ( isset($meta['product_videos'])) { ?>
 						<div class="video">
 						<table id="video">
 							<tr>
 								<th colspan="2">Video</th>
-								
-							</tr>	
+
+							</tr>
 							<tr>
 								<td colspan="2">
 									<?php echo $meta['product_videos']; ?>
 
 								</td>
-							</tr>	
+							</tr>
 						</table>
 						</div>
-						<?php } // only show geometry section if video value is set ?>
-						
+						<?php
+} // only show geometry section if video value is set ?>
+
 						<?php
 							$meta = $product_metabox->the_meta();
 							if ( isset($meta['products'])) {
@@ -46,7 +49,7 @@
 						<table id="specs">
 							<tr>
 								<th>Specs</th>
-							</tr>		
+							</tr>
 							<?php
 								// the product spec
 								if ( isset($meta['products'])) {
@@ -64,14 +67,14 @@
 							}
 						} // end if product spec isset
 						?>
-						
+
 						<?php if ( isset($meta['geometry'])) { ?>
 						<div class="geometry">
 						<table id="geometry">
 							<tr>
 								<th colspan="2"><?php if ( has_term( 'complete-bikes', 'type', $post->ID ) ) { echo 'Specs'; } else { echo 'Geometry'; } ?></th>
-								
-							</tr>		
+
+							</tr>
 							<?php
 								// the product spec
 								foreach ($meta['geometry'] as $detail) {
@@ -83,18 +86,59 @@
 							?>
 						</table>
 						</div>
-						<?php } // only show geometry section if video value is set ?>
-
-						
-					
+						<?php
+} // only show geometry section if video value is set
+?>
 					</div><!-- .entry-content -->
-	
+
 				</article>
 
 			<?php endwhile; // end of the loop. ?>
-
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
+<?php
+        if ( 'products' == get_post_type() ) {
+            $taxs = wp_get_post_terms( $post->ID, 'type' );
+            if ( $taxs ) {
+                $tax_ids = array();
+                foreach( $taxs as $tax)
+                    $tax_ids[] = $tax->term_id;
+                    $args = array(
+                        'tax_query' => array(
+                            array(
+                                'taxonomy'  => 'type',
+                                'terms'     => $tax_ids,
+                                'operator'  => 'IN'
+                            )
+                        ),
+                        'post__not_in'          => array( $post->ID ),
+                        'posts_per_page'        => 3,
+                        'ignore_sticky_posts'   => 1
+                    );
+
+                $my_query = new wp_query( $args );
+                if( $my_query->have_posts() ) {
+                    echo '<div class="related-products">';
+                    echo '<h3>Related Products</h3>';
+                    while ( $my_query->have_posts() ) :
+                        $my_query->the_post();
+                        echo '<div class="related-product-wrapper">';
+                        echo '<a href="'.get_permalink().'" rel="bookmark" class="product-related">';
+                        the_post_thumbnail();
+                        the_title('<span class="product-related-title">','</span>');
+                        echo '</a>';
+                        echo '</div>';
+                    endwhile;
+                    wp_reset_query();
+
+                    echo '</div>';
+            }
+          }
+      }
+?>
 
 <?php get_footer(); ?>
+
+
+
